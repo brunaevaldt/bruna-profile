85d4a082e004d550e6bd102c69098422633ab827
Giwan Persaud <giwan.persaud@gmail.com>
2020-11-17 20:41:22 +0100

    scrum master responsive

--- components/ScrumMaster/ScrumMaster.js
+++ components/ScrumMaster/ScrumMaster.js
@@ -1,24 +1,22 @@
 import PostItStack from "./PostItStack";
 import styles from "./scrumMaster.module.css";
 
-const ScrumMaster = () => {
-    return (
-        <section className={styles.scrumMasterContainer}>
-            <h2>Scrum Master</h2>
-            <PostItStack />
-            <div>
-                <p>
-                    Strive for clean maintainable code. Don’t push to production
-                    on Friday. People over process.{" "}
-                </p>
-                <p>
-                    These are not just empty idioms. They help guide the team
-                    when everyone is working hard to make that next deadline.
-                    They help guide the quality process.{" "}
-                </p>
-            </div>
-        </section>
-    );
-};
+const ScrumMaster = () => (
+    <section className={styles.scrumMasterContainer}>
+        <h2>Scrum Master</h2>
+        <PostItStack />
+        <div>
+            <p>
+                Strive for clean maintainable code. Don’t push to production on
+                Friday. People over process.{" "}
+            </p>
+            <p>
+                These are not just empty idioms. They help guide the team when
+                everyone is working hard to make that next deadline. They help
+                guide the quality process.{" "}
+            </p>
+        </div>
+    </section>
+);
 
 export default ScrumMaster;
