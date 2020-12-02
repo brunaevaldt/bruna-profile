5c5f97d2be5093dc7b1d79ce3412294477962927
Giwan Persaud <giwan.persaud@gmail.com>
2020-12-02 19:17:09 +0100

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cc25857c2c446887251e";
+/******/ 	var hotCurrentHash = "609165560efeaa54e081";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
